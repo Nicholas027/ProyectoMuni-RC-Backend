f58f6b920512724f0aa0a2408200aa7d24ab3d77
Nicholas027 <nicholas0810152015@gmail.com>
2024-04-13 16:22:23 -0300

    creamos los esquemas de mongoose para el profesional y el usuario, ambos con validaciones

---
+++ src/database/models/user.js
@@ -0,0 +1,42 @@
+import mongoose, {Schema} from "mongoose";
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    admin: {
+        type: Boolean,
+        required: false
+    }
+})
+
+//Hasheo de contraseña
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(parseInt(process.env.HASH_SALTS), function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+//vamos a generar el modelo Usuario
+const User = mongoose.model('user', userSchema)
+
+export default User;
