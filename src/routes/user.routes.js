f515ede35febcbc494bc56241dbb5052f6943a5f
Alejo Herrera <alejoherrera1166@gmail.com>
2024-05-04 10:50:12 -0300

    Agregué la ruta  para la solicitud GET de usuarios.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { userRegister } from "../controllers/user.controllers.js";
+import { userRegister, usersList } from "../controllers/user.controllers.js";
 import validacionUser from "../helpers/validacionUser.js";
 
 const routerUser = Router();
 
 routerUser.route("/users/register").post([validacionUser], userRegister);
+routerUser.route("/users").ger(usersList);
+
 
 export default routerUser;
